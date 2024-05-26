40e7c94deb910ef35336cf128681eed81efdc52a
JNCamil <juancamvh@hotmail.com>
2024-05-26 17:40:23 +0200

    formularios y controles

---
+++ ConversionTipos/ConversionTipos/FormVentana.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormVentana
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCerrar = New System.Windows.Forms.Button()
+        Me.btnAbrir = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnCerrar
+        '
+        Me.btnCerrar.Location = New System.Drawing.Point(244, 124)
+        Me.btnCerrar.Name = "btnCerrar"
+        Me.btnCerrar.Size = New System.Drawing.Size(215, 135)
+        Me.btnCerrar.TabIndex = 0
+        Me.btnCerrar.Text = "CERRAR"
+        Me.btnCerrar.UseVisualStyleBackColor = True
+        '
+        'btnAbrir
+        '
+        Me.btnAbrir.Location = New System.Drawing.Point(517, 114)
+        Me.btnAbrir.Name = "btnAbrir"
+        Me.btnAbrir.Size = New System.Drawing.Size(215, 135)
+        Me.btnAbrir.TabIndex = 1
+        Me.btnAbrir.Text = "ABRIR FORM1"
+        Me.btnAbrir.UseVisualStyleBackColor = True
+        '
+        'FormVentana
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.InfoText
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnAbrir)
+        Me.Controls.Add(Me.btnCerrar)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Name = "FormVentana"
+        Me.Text = "VETANA 3"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnCerrar As Button
+    Friend WithEvents btnAbrir As Button
+End Class
---
+++ ConversionTipos/ConversionTipos/FormVentana.vb
@@ -0,0 +1,38 @@
+﻿Imports System.ComponentModel
+
+Public Class FormVentana
+    'EVENTOS: -------------------------------------------------------------------------------------------------------------------'
+    'Evento que se genera por defecto cuando se carga el formulario:'
+    Private Sub FormVentana_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Me.BackColor = Color.White 'me: hace referencia al formulario(objeto), es decir, sustituye a FormVentana'
+    End Sub
+
+    Private Sub FormVentana_MouseClick(sender As Object, e As MouseEventArgs) Handles Me.MouseClick
+        'MsgBox("HAZ DADO CLICK AL FORMULARIO")'
+    End Sub
+
+    Private Sub FormVentana_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        ' MsgBox("FORMULARIO SE ESTÁ CERRANDO")'
+    End Sub
+
+    Private Sub FormVentana_DoubleClick(sender As Object, e As EventArgs) Handles Me.DoubleClick
+        ' MsgBox("HAZ HECHO DOBLE CLICK EN EL FORMULARIO") 'Toma el evento click antes, habría que comentar el de 1 click'
+    End Sub
+
+    Private Sub FormVentana_Move(sender As Object, e As EventArgs) Handles Me.Move
+        ' MsgBox("HAZ MOVIDO EL FORMULARIO")'
+    End Sub
+
+
+    'MÉTODOS: -------------------------------------------------------------------------------------------------------------------'
+
+    Private Sub btnCerrar_Click(sender As Object, e As EventArgs) Handles btnCerrar.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnAbrir.Click
+        ' Form1.Show()
+        Form1.ShowDialog() 'Que lo ponga de manera modal, por encima, hasta que no cierre nada, se superpone
+
+    End Sub
+End Class
