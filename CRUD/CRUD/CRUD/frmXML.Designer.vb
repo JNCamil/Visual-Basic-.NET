b76f25bb2f9f997c7a270e94e6dbbabdb9f5a78a
JNCamil <juancamvh@hotmail.com>
2024-05-28 19:43:24 +0200

    Vb fin curso + proyecto

---
+++ CRUD/CRUD/CRUD/frmXML.Designer.vb
@@ -0,0 +1,185 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmXML
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtid = New System.Windows.Forms.TextBox()
+        Me.txtnombre = New System.Windows.Forms.TextBox()
+        Me.txtprecio = New System.Windows.Forms.TextBox()
+        Me.chkEstado = New System.Windows.Forms.CheckBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnNuevo = New System.Windows.Forms.Button()
+        Me.btninserta = New System.Windows.Forms.Button()
+        Me.btnmodifica = New System.Windows.Forms.Button()
+        Me.btnelimina = New System.Windows.Forms.Button()
+        Me.dgvDatos = New System.Windows.Forms.DataGridView()
+        CType(Me.dgvDatos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtid
+        '
+        Me.txtid.Enabled = False
+        Me.txtid.Location = New System.Drawing.Point(262, 121)
+        Me.txtid.Name = "txtid"
+        Me.txtid.Size = New System.Drawing.Size(100, 22)
+        Me.txtid.TabIndex = 0
+        '
+        'txtnombre
+        '
+        Me.txtnombre.Location = New System.Drawing.Point(262, 178)
+        Me.txtnombre.Name = "txtnombre"
+        Me.txtnombre.Size = New System.Drawing.Size(100, 22)
+        Me.txtnombre.TabIndex = 1
+        '
+        'txtprecio
+        '
+        Me.txtprecio.Location = New System.Drawing.Point(262, 235)
+        Me.txtprecio.Name = "txtprecio"
+        Me.txtprecio.Size = New System.Drawing.Size(100, 22)
+        Me.txtprecio.TabIndex = 2
+        '
+        'chkEstado
+        '
+        Me.chkEstado.AutoSize = True
+        Me.chkEstado.Location = New System.Drawing.Point(262, 305)
+        Me.chkEstado.Name = "chkEstado"
+        Me.chkEstado.Size = New System.Drawing.Size(80, 21)
+        Me.chkEstado.TabIndex = 4
+        Me.chkEstado.Text = "ACTIVO"
+        Me.chkEstado.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(147, 126)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(19, 17)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "id"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(147, 178)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 17)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "nombre"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(147, 238)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 17)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "precio"
+        '
+        'btnNuevo
+        '
+        Me.btnNuevo.Location = New System.Drawing.Point(433, 92)
+        Me.btnNuevo.Name = "btnNuevo"
+        Me.btnNuevo.Size = New System.Drawing.Size(103, 55)
+        Me.btnNuevo.TabIndex = 8
+        Me.btnNuevo.Text = "Nuevo"
+        Me.btnNuevo.UseVisualStyleBackColor = True
+        '
+        'btninserta
+        '
+        Me.btninserta.Location = New System.Drawing.Point(433, 178)
+        Me.btninserta.Name = "btninserta"
+        Me.btninserta.Size = New System.Drawing.Size(103, 55)
+        Me.btninserta.TabIndex = 9
+        Me.btninserta.Text = "insertar"
+        Me.btninserta.UseVisualStyleBackColor = True
+        '
+        'btnmodifica
+        '
+        Me.btnmodifica.Location = New System.Drawing.Point(433, 271)
+        Me.btnmodifica.Name = "btnmodifica"
+        Me.btnmodifica.Size = New System.Drawing.Size(103, 55)
+        Me.btnmodifica.TabIndex = 10
+        Me.btnmodifica.Text = "modificar"
+        Me.btnmodifica.UseVisualStyleBackColor = True
+        '
+        'btnelimina
+        '
+        Me.btnelimina.Location = New System.Drawing.Point(433, 361)
+        Me.btnelimina.Name = "btnelimina"
+        Me.btnelimina.Size = New System.Drawing.Size(103, 55)
+        Me.btnelimina.TabIndex = 11
+        Me.btnelimina.Text = "eliminar"
+        Me.btnelimina.UseVisualStyleBackColor = True
+        '
+        'dgvDatos
+        '
+        Me.dgvDatos.AllowUserToAddRows = False
+        Me.dgvDatos.AllowUserToDeleteRows = False
+        Me.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvDatos.Location = New System.Drawing.Point(712, 92)
+        Me.dgvDatos.Name = "dgvDatos"
+        Me.dgvDatos.ReadOnly = True
+        Me.dgvDatos.RowHeadersWidth = 51
+        Me.dgvDatos.RowTemplate.Height = 24
+        Me.dgvDatos.Size = New System.Drawing.Size(666, 300)
+        Me.dgvDatos.TabIndex = 12
+        '
+        'frmXML
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1508, 741)
+        Me.Controls.Add(Me.dgvDatos)
+        Me.Controls.Add(Me.btnelimina)
+        Me.Controls.Add(Me.btnmodifica)
+        Me.Controls.Add(Me.btninserta)
+        Me.Controls.Add(Me.btnNuevo)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.chkEstado)
+        Me.Controls.Add(Me.txtprecio)
+        Me.Controls.Add(Me.txtnombre)
+        Me.Controls.Add(Me.txtid)
+        Me.Name = "frmXML"
+        Me.Text = "Form1"
+        CType(Me.dgvDatos, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtid As TextBox
+    Friend WithEvents txtnombre As TextBox
+    Friend WithEvents txtprecio As TextBox
+    Friend WithEvents chkEstado As CheckBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnNuevo As Button
+    Friend WithEvents btninserta As Button
+    Friend WithEvents btnmodifica As Button
+    Friend WithEvents btnelimina As Button
+    Friend WithEvents dgvDatos As DataGridView
+End Class
---
+++ CRUD/CRUD/CRUD/frmXML.vb
@@ -0,0 +1,156 @@
+﻿Imports System.Xml 'Importar la librería para trabajar con XML
+Public Class frmXML
+    Dim ruta As String = AppDomain.CurrentDomain.BaseDirectory & "producto.xml" 'Es decir bin - Debug
+
+    Dim docXml As New XmlDocument 'Creamos el objeto Xml para modificar la información
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvDatos.CellContentClick
+
+    End Sub
+
+    Private Sub frmXML_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        listarProductos()
+
+        docXml.Load(ruta) 'Inicializamos el objeto de la ruta
+
+    End Sub
+
+    Sub listarProductos()
+        'NECESITAMOS UN ORIGEN DE DATOS O DATA SOURCE: UNO DE ELLOS PUEDE SER EL DATA TABLE
+        Dim tablaProductos As New DataTable("producto")
+        tablaProductos.Columns.Add("id", GetType(Integer))
+        tablaProductos.Columns.Add("nombre", GetType(String))
+        tablaProductos.Columns.Add("precio", GetType(Double))
+        tablaProductos.Columns.Add("estado", GetType(Boolean))
+
+        'Leer el archivo XML
+        tablaProductos.ReadXml(ruta)
+
+        'Poner datos en datagrid - Origen de los datos:
+        dgvDatos.DataSource = tablaProductos
+
+
+    End Sub
+    'INSERTAR-----------------------------------------------------------------------------------------------------------------------------------
+    Private Sub btninserta_Click(sender As Object, e As EventArgs) Handles btninserta.Click
+
+        'CONTROL
+        If txtid.Text <> "" Then
+            MsgBox("Presione el botón nuevo para hacer un nuevo registro")
+            Exit Sub
+        End If
+
+        Dim ultimoNodo As XmlNode = docXml.DocumentElement.LastChild 'Capturamos el último nodo producto
+
+        Dim producto As XmlElement = docXml.CreateElement("producto")
+
+        Dim id As XmlElement = docXml.CreateElement("id")
+        Dim nombre As XmlElement = docXml.CreateElement("nombre")
+        Dim precio As XmlElement = docXml.CreateElement("precio")
+        Dim estado As XmlElement = docXml.CreateElement("estado")
+
+        'Insertamos los elementos
+
+        id.InnerText = Integer.Parse(ultimoNodo.ChildNodes(0).InnerText) + 1 'ChildNodes - todos los nodos (0) -> id 
+        nombre.InnerText = Trim(txtnombre.Text) ' Función trim para quitar los espacios
+        precio.InnerText = Trim(txtprecio.Text) ' Función trim para quitar los espacios
+        estado.InnerText = IIf(chkEstado.Checked = True, "1", "0") 'Iff Evalúa condición 1 - true, 0 - false
+
+        'Insertamos los nodos al padre
+        producto.AppendChild(id)
+        producto.AppendChild(nombre)
+        producto.AppendChild(precio)
+        producto.AppendChild(estado)
+
+        'Insertar el nodo
+        docXml.DocumentElement.InsertAfter(producto, ultimoNodo)
+        docXml.Save(ruta) ' Guardamos
+
+
+        'LIMPIAR CAMPOS
+        txtnombre.Text = ""
+        txtprecio.Text = ""
+        MsgBox("Registro insertado correctamente")
+        'Actualizamos el DataGrid
+        Call listarProductos()
+
+
+
+
+    End Sub
+
+    'MODIFICAR-----------------------------------------------------------------------------------------------------------------------------------
+    Private Sub dgvDatos_SelectionChanged(sender As Object, e As EventArgs) Handles dgvDatos.SelectionChanged
+        'SI HAY DATOS:
+        If dgvDatos.Rows.Count > 0 Then
+            'DETERMINAR SI HAY ALGUNA FILA SELECCIONADA:
+            If Not IsNothing(dgvDatos.CurrentRow) Then
+                txtid.Text = dgvDatos.CurrentRow.Cells(0).Value 'Celda 0 para el id
+                txtnombre.Text = dgvDatos.CurrentRow.Cells(1).Value
+                txtprecio.Text = dgvDatos.CurrentRow.Cells(2).Value
+                chkEstado.Checked = Boolean.Parse(dgvDatos.CurrentRow.Cells(3).Value)
+
+
+            End If
+        End If
+    End Sub
+
+    Private Sub btnmodifica_Click(sender As Object, e As EventArgs) Handles btnmodifica.Click
+
+        'CONTROL
+        If txtid.Text = "" Then
+            MsgBox("Debe seleccionar el producto a modificar")
+            Exit Sub
+        End If
+
+        Dim producto As XmlElement = docXml.DocumentElement.SelectSingleNode( _ 'el guión bajo _ se usa para continuar en la siguiente línea
+           String.Format("producto[id='{0}']", txtid.Text)) ' SELECCIONAMOS EL ID del nodo el cual hayamos especificado en el DataGrid
+
+        producto.ChildNodes(1).InnerText = txtnombre.Text
+        producto.ChildNodes(2).InnerText = txtprecio.Text
+        producto.ChildNodes(3).InnerText = IIf(chkEstado.Checked = True, "1", "0")
+        docXml.Save(ruta)
+        MsgBox("Registro modificado correctamente")
+        Call listarProductos()
+
+
+    End Sub
+
+    'ELIMINAR-----------------------------------------------------------------------------------------------------------------------------------
+
+    Private Sub btnelimina_Click(sender As Object, e As EventArgs) Handles btnelimina.Click
+
+        'CONTROL
+        If txtid.Text = "" Then
+            MsgBox("Debe seleccionar el producto a eliminar")
+            Exit Sub
+        End If
+
+        'NODO RAÍZ 
+        Dim raiz As XmlElement = docXml.DocumentElement
+        Dim producto As XmlElement = docXml.DocumentElement.SelectSingleNode( _ 'el guión bajo _ se usa para continuar en la siguiente línea
+           String.Format("producto[id='{0}']", txtid.Text)) ' SELECCIONAMOS EL ID del nodo el cual hayamos especificado en el DataGrid
+
+        Dim respuesta
+        respuesta = MsgBox("Seguro que desea eliminar este registro?", MsgBoxStyle.Question + MsgBoxStyle.YesNo)
+        If respuesta = vbYes Then
+
+        Else
+            Exit Sub
+        End If
+
+
+        raiz.RemoveChild(producto)
+        docXml.Save(ruta)
+        MsgBox("El registro se ha eliminado correctamente")
+        Call listarProductos()
+
+    End Sub
+
+    Private Sub btnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
+        txtid.Text = ""
+        txtnombre.Text = ""
+        txtprecio.Text = ""
+        chkEstado.Checked = False
+    End Sub
+End Class
